51786bad58303c3300e3d489ce9a2adca2f11b79
Muhammad <mf9555759@gmail.com>
2025-11-03 19:53:45 +0800

    github

---
+++ main.ts
@@ -0,0 +1,42 @@
+
+const techContainer = document.getElementById("tech-container")!;
+const button = document.getElementById("action-btn")!;
+
+const techStack = [
+    "HTML", "CSS", "JavaScript", "TypeScript",
+    "React", "Next.js", "TailwindCSS",
+    "Node.js", "Express", "MySQL"
+];
+
+let index = 0;
+function showSkills() {
+    if (index < techStack.length) {
+        const skill = techStack[index];
+        const span = document.createElement("span");
+        span.textContent = `⚡ ${skill}`;
+        span.className =
+            "block text-lg text-gray-200 opacity-0 translate-y-3 transition-all duration-500";
+        techContainer.appendChild(span);
+
+        setTimeout(() => {
+            span.classList.remove("opacity-0", "translate-y-3");
+            span.classList.add("opacity-100", "translate-y-0");
+        }, 100);
+
+        index++;
+        setTimeout(showSkills, 300);
+    }
+}
+showSkills();
+
+button.addEventListener("click", () => {
+    const link = "https://youtu.be/dQw4w9WgXcQ"; 
+    button.textContent = "🔄 Redirecting...";
+    button.classList.add("scale-95");
+
+    setTimeout(() => {
+        window.open(link, "_blank");
+        button.textContent = "🔗 Visit My Link";
+        button.classList.remove("scale-95");
+    }, 800);
+});
